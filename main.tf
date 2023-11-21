09e3c27f9a97555425d327f1c679babca1c91e29
anas-cr7 <anwaralianas07@gmail.com>
2023-11-21 15:29:32 +0530

    updating

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 resource "local_file" "pet" {
 filename = "/root/pets.txt"
-content = "My cat is MR.Cat"
-}
+content = "My cat is MR.CAT"
+depends_on = [
+random_pet.mypet
+] 
+} 
 resource "random_pet" "mypet" {
 prefix = "MR"
 separator = "."
 length = "1"
 }
+
+
+
