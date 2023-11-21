4208efe138fc60580d1d6438c5bd58da2891ea5c
anas-cr7 <anwaralianas07@gmail.com>
2023-11-21 15:48:51 +0530

    dding new

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
 resource "local_file" "pet" {
-filename = "/root/pets.txt"
-content = "My cat is MR.CAT"
-depends_on = [
-random_pet.mypet
-] 
-} 
-resource "random_pet" "mypet" {
-prefix = "MR"
-separator = "."
-length = "1"
+    filename = "/pets.txt"
+    content = "MY cat is MR.CAT"
+    lifecycle {
+      create_before_destroy = true
+    }
+  
 }
-
-
-
