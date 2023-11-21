a1c8e2c5875d962a38faeb0b35b7a091ca0a5550
anas-cr7 <anwaralianas07@gmail.com>
2023-11-21 15:59:36 +0530

    updating

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "local_file" "pet" {
-    filename = "/pets.txt"
-    content = "MY cat is MR.CAT"
-    lifecycle {
-      create_before_destroy = true
-    }
-  
+filename = "pets.txt"
+content = "we love pets!"
+}
+resource "random_pet" "mypet" {
+prefix = "MR"
+separator = "."
+length = "1"
 }
