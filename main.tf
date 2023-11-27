524847146c05e5f0407e68bfcb33e13faaea15d1
anas-cr7 <anwaralianas07@gmail.com>
2023-11-27 16:16:13 +0530

    aws

--- main.tf
+++ main.tf
@@ -1,9 +1,102 @@
-resource "local_file" "pet" {
-filename = var.filename
-content = var.content
+provider "aws" {
+  region = "us-east-1"  
 }
-resource "random_pet" "mypet" {
-prefix = var.prefix
-separator = "."
-length = var.length
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"  
+
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"  
+  availability_zone       = "us-east-1a"   
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyIGW"
+  }
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "MySecurityGroup"
+  description = "Security group allowing SSH, HTTP, and HTTPS traffic"
+
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
+resource "aws_network_interface" "my_network_interface" {
+  subnet_id          = aws_subnet.my_subnet.id
+  private_ips        = ["10.0.1.10"]
+
+  security_groups = [aws_security_group.my_security_group.id]
+
+  tags = {
+    Name = "MyNetworkInterface"
+  }
 }
